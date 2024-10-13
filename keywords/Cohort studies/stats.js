320012aec767b0c7c4bb0f87315d8deb56b9ff17
Azure pipeline <you@example.com>
2024-10-13 01:34:45 +0000

    Automatically updating web files for datashield

--- keywords/Cohort studies/stats.js
+++ keywords/Cohort studies/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2018',1],]);var citations =([['Year', 'Number of Citations'],['2018',23],]);
+var papers =([['Year', 'Number of papers'],['2018',1],['2019',0],['2020',0],['2021',0],['2022',0],['2023',1],]);var citations =([['Year', 'Number of Citations'],['2018',23],['2019',0],['2020',0],['2021',0],['2022',0],['2023',4],]);
